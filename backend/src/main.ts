f5e370dedbd7e5afa0764755e273c1f73a687f9b
Dan Parfenov <prostodan4ikn@ya.ru>
2023-05-21 12:56:39 +0300

    Add prisma

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import { PrismaService } from './modules/database/services/prisma.service'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { cors: true })
   app.setGlobalPrefix('api')
+  const prismaService = app.get(PrismaService)
+  await prismaService.enableShutdownHooks(app)
   await app.listen(3000)
   console.log(`App is listening on port 3000`)
 }
